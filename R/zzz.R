432f1faa1600743a0a4b2800a7aa9ab79aa6fa86
Evan Muise <evanmuise@gmail.com>
2026-02-13 11:37:42 -0800

    Initial commit

---
+++ R/zzz.R
@@ -0,0 +1,35 @@
+#' Initialize muiselib defaults
+#' 
+#' This function sets the global ggplot2 theme and default color palettes
+#' to the muiselib standard.
+#' 
+#' @export
+muiselib_setup <- function() {
+  
+  # 1. Set the Global ggplot2 Theme
+  main_theme <- ggplot2::theme_bw() +
+                ggplot2::theme(panel.grid = ggplot2::element_blank())
+  
+  ggplot2::theme_set(main_theme)
+
+  # 2. Define custom palettes
+  pal_list <- list(
+    c("#4575b4", "#66bd63"),
+    RColorBrewer::brewer.pal(3, "Set2"),
+    c("#4053d3", "#ddb310", "#b51d14", "#00beff", "#fb49b0"),
+    RColorBrewer::brewer.pal(6, "Accent")
+  )
+
+  options(
+    ggplot2.discrete.colour = pal_list,
+    ggplot2.discrete.fill = pal_list
+  )
+  
+  message("muiselib: ggplot2 theme and palettes successfully configured.")
+}
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    "muiselib loaded. Run 'muiselib_setup()' to apply default themes and palettes."
+  )
+}
